6d1e0fd610c23680d1535ad098e66cef0b0d17f9
zhekai3322 <23024477@myrp.edu.sg>
2024-11-15 09:36:55 +0800

    Ex 1

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Ex 1
+import React from 'react';
+import { View, FlatList, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const datasource = [
+  { key: 'a' },
+  { key: 'b' },
+  { key: 'c' },
+  { key: 'd' },
+  { key: 'e' },
+  { key: 'f' },
+  { key: 'g' },
+  { key: 'h' },
+  { key: 'i' },
+  { key: 'j' },
+  { key: 'k' },
+  { key: 'l' },
+  { key: 'm' },
+  { key: 'n' },
+  { key: 'o' },
+  { key: 'p' },
+  { key: 'q' },
+  { key: 'r' },
+  { key: 's' },
+  { key: 't' },
+];
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  opacityStyle: {
+    borderWidth: 1,
+    padding: 8,
+  },
+  textStyle: {
+    fontSize: 15,
+    margin: 10,
+    textAlign: 'left',
   },
 });
+
+const renderItem = ({ item }) => {
+  return (
+      <TouchableOpacity style={styles.opacityStyle}>
+        <Text style={styles.textStyle}>{item.key}</Text>
+      </TouchableOpacity>
+  );
+};
+
+const App = () => {
+  return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+            data={datasource}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.key}
+        />
+      </View>
+  );
+};
+
+export default App;
